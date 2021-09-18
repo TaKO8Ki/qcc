33c1328cbf4369d6657886519949b08f1bd0d9a1
Takayuki Maeda <takoyaki0316@gmail.com>
2021-09-18 23:09:31 +0900

    add sub

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let arg = env::args().nth(1).unwrap();
+
+    let mut chars = arg.chars();
+
+    println!(".intel_syntax noprefix");
+    println!(".globl main");
+    println!("main:");
+
+    let mut expr = Vec::new();
+
+    let mut is_number = false;
+    let mut number = vec![];
+    loop {
+        if let Some(c) = chars.next() {
+            if c.is_digit(10) {
+                is_number = true;
+                number.push(c)
+            } else if is_number {
+                expr.push(number.iter().collect::<String>());
+                expr.push(c.to_string());
+                number.clear();
+            } else {
+                expr.push(c.to_string());
+            }
+        } else {
+            expr.push(number.iter().collect::<String>());
+            break;
+        }
+    }
+
+    let mut expr_iter = expr.iter();
+    println!("  mov rax, {}", expr_iter.next().unwrap());
+
+    while let Some(c) = expr_iter.next() {
+        let next = expr_iter.next().unwrap();
+
+        if c.as_str() == "+" {
+            println!("  add rax, {}", next);
+            continue;
+        }
+
+        if c.as_str() == "-" {
+            println!("  sub rax, {}", next);
+            continue;
+        }
+
+        panic!("not expected value: {}", c);
+    }
+
+    println!("  ret");
 }
---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+assert() {
+  expected="$1"
+  input="$2"
+
+  ./target/debug/qcc "$input" > tmp.s
+  cc -o tmp tmp.s
+  ./tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+assert 0 0
+assert 42 42
+assert 21 "5+20-4"
+
+echo OK
