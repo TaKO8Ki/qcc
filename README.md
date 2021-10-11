296a4346a017cc6173fcbd802ea4dc0599cfac51
Takayuki Maeda <takoyaki0316@gmail.com>
2021-10-12 05:12:12 +0900

    tokenizer

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # qcc
 C compiler written in Rust
+
+```sh
+docker run --rm -it --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/myapp -w /usr/src/myapp rust ./test.sh
+```
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,135 @@
 use std::env;
 
-fn main() {
+#[derive(PartialEq, Clone, Debug)]
+enum TokenKind {
+    Reserved,
+    Num(u16),
+    Eof,
+}
+
+#[derive(Debug, Clone)]
+struct Token {
+    kind: TokenKind,
+    str: char,
+}
+
+impl Token {
+    fn new(kind: TokenKind, str: char) -> Self {
+        let tok = Self { kind, str };
+        tok
+    }
+
+    fn consume(&self, op: char) -> bool {
+        if self.kind != TokenKind::Reserved || self.str != op {
+            return false;
+        }
+        true
+    }
+
+    fn expect(&self, op: char) -> bool {
+        if self.kind != TokenKind::Reserved || self.str != op {
+            return false;
+        }
+        true
+    }
+
+    fn expect_number(&self) -> Result<u16, String> {
+        if let TokenKind::Num(val) = self.kind {
+            Ok(val)
+        } else {
+            Err(format!("{} is not number", self.str))
+        }
+    }
+
+    fn at_eof(&self) -> bool {
+        self.kind == TokenKind::Eof
+    }
+
+    fn tokenize(p: String) -> Result<Vec<Token>, String> {
+        let mut tokens = vec![];
+        let mut chars = p.chars();
+
+        while let Some(p) = chars.next() {
+            if p.is_whitespace() {
+                continue;
+            }
+
+            if p == '+' || p == '-' {
+                tokens.push(Self::new(TokenKind::Reserved, p));
+                continue;
+            }
+
+            if p.is_digit(10) {
+                let mut number = vec![p];
+                let mut op = None;
+                while let Some(c) = chars.next() {
+                    if !c.is_digit(10) {
+                        if !c.is_whitespace() {
+                            op = Some(c);
+                        }
+                        break;
+                    }
+                    number.push(c);
+                }
+                tokens.push(Self::new(
+                    TokenKind::Num(number.iter().collect::<String>().parse::<u16>().or_else(
+                        |_| Err(format!("cannot convert char to integer: {:?}", number)),
+                    )?),
+                    p,
+                ));
+                if let Some(op) = op {
+                    tokens.push(Self::new(TokenKind::Reserved, op));
+                }
+                continue;
+            }
+
+            return Err("cannot tokenize".to_string());
+        }
+
+        Self::new(TokenKind::Eof, '\0');
+        Ok(tokens)
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let arg = env::args().nth(1).unwrap();
 
-    let mut chars = arg.chars();
+    let chars = arg.chars();
 
     println!(".intel_syntax noprefix");
     println!(".globl main");
     println!("main:");
 
-    let mut expr = Vec::new();
+    let tokens = Token::tokenize(chars.collect::<String>()).unwrap();
+    let mut tokens_iter = tokens.iter();
 
-    let mut is_number = false;
-    let mut number = vec![];
-    loop {
-        if let Some(c) = chars.next() {
-            if c.is_digit(10) {
-                is_number = true;
-                number.push(c)
-            } else if is_number {
-                expr.push(number.iter().collect::<String>());
-                expr.push(c.to_string());
-                number.clear();
-            } else {
-                expr.push(c.to_string());
-            }
-        } else {
-            expr.push(number.iter().collect::<String>());
+    // println!("tokens: {:?}", tokens);
+
+    println!(
+        "  mov rax, {}",
+        tokens_iter.next().unwrap().expect_number()?
+    );
+
+    while let Some(token) = tokens_iter.next() {
+        if token.at_eof() {
             break;
         }
-    }
-
-    let mut expr_iter = expr.iter();
-    println!("  mov rax, {}", expr_iter.next().unwrap());
-
-    while let Some(c) = expr_iter.next() {
-        let next = expr_iter.next().unwrap();
-
-        if c.as_str() == "+" {
-            println!("  add rax, {}", next);
+        if token.consume('+') {
+            if let Some(token) = tokens_iter.next() {
+                println!("  add rax, {}", token.expect_number()?);
+            }
             continue;
         }
 
-        if c.as_str() == "-" {
-            println!("  sub rax, {}", next);
-            continue;
-        }
-
-        panic!("not expected value: {}", c);
+        if token.expect('-') {
+            if let Some(token) = tokens_iter.next() {
+                println!("  sub rax, {}", token.expect_number()?);
+            }
+        } else {
+            println!("  sub rax, {}", token.expect_number()?);
+        };
     }
 
     println!("  ret");
+    Ok(())
 }
