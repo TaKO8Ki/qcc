73bea2e944480036f43a86c55d415b752137515d
Takayuki Maeda <takoyaki0316@gmail.com>
2021-11-15 16:43:56 +0900

    add comparison operators

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,10 @@ enum NodeKind {
     Sub,
     Mul,
     Div,
+    Eq,
+    Ne,
+    Lt,
+    Le,
     Num(u16),
 }
 
@@ -25,7 +29,7 @@ struct Tokens {
 #[derive(Debug, Clone)]
 struct Token {
     kind: TokenKind,
-    str: char,
+    str: String,
 }
 
 struct Node {
@@ -93,6 +97,26 @@ impl Node {
                 asm.push(String::from("  cqo"));
                 asm.push(String::from("  idiv rdi"));
             }
+            NodeKind::Eq => {
+                asm.push(String::from("  cmp rax, rdi"));
+                asm.push(String::from("  sete al"));
+                asm.push(String::from("  movzb rax, al"));
+            }
+            NodeKind::Ne => {
+                asm.push(String::from("  cmp rax, rdi"));
+                asm.push(String::from("  setne al"));
+                asm.push(String::from("  movzb rax, al"));
+            }
+            NodeKind::Lt => {
+                asm.push(String::from("  cmp rax, rdi"));
+                asm.push(String::from("  setl al"));
+                asm.push(String::from("  movzb rax, al"));
+            }
+            NodeKind::Le => {
+                asm.push(String::from("  cmp rax, rdi"));
+                asm.push(String::from("  setle al"));
+                asm.push(String::from("  movzb rax, al"));
+            }
             _ => {}
         }
 
@@ -115,12 +139,16 @@ impl Tokens {
     }
 
     fn expr(&mut self) -> Node {
+        self.equality()
+    }
+
+    fn add(&mut self) -> Node {
         let mut node = self.mul();
 
         loop {
-            if self.consume('+') {
+            if self.consume("+") {
                 node = Node::new(NodeKind::Add, node, self.mul());
-            } else if self.consume('-') {
+            } else if self.consume("-") {
                 node = Node::new(NodeKind::Sub, node, self.mul());
             } else {
                 return node;
@@ -132,9 +160,9 @@ impl Tokens {
         let mut node = self.unary();
 
         loop {
-            if self.consume('*') {
+            if self.consume("*") {
                 node = Node::new(NodeKind::Mul, node, self.unary());
-            } else if self.consume('/') {
+            } else if self.consume("/") {
                 node = Node::new(NodeKind::Div, node, self.unary());
             } else {
                 return node;
@@ -143,27 +171,67 @@ impl Tokens {
     }
 
     fn unary(&mut self) -> Node {
-        if self.consume('+') {
+        if self.consume("+") {
             return self.primary();
-        } else if self.consume('-') {
+        } else if self.consume("-") {
             return Node::new(NodeKind::Sub, Node::new_node_num(0), self.primary());
         }
         self.primary()
     }
 
     fn primary(&mut self) -> Node {
-        if self.consume('(') {
+        if self.consume("(") {
             let node = self.expr();
-            self.consume(')');
+            self.expect(")");
             return node;
         }
 
         Node::new_node_num(self.expect_number().unwrap())
     }
 
-    fn consume(&mut self, op: char) -> bool {
+    fn equality(&mut self) -> Node {
+        let mut node = self.relational();
+
+        loop {
+            if self.consume("==") {
+                node = Node::new(NodeKind::Eq, node, self.relational());
+            } else if self.consume("!=") {
+                node = Node::new(NodeKind::Ne, node, self.relational());
+            } else {
+                return node;
+            }
+        }
+    }
+
+    fn relational(&mut self) -> Node {
+        let mut node = self.add();
+
+        loop {
+            if self.consume("<") {
+                node = Node::new(NodeKind::Lt, node, self.add());
+            } else if self.consume("<=") {
+                node = Node::new(NodeKind::Le, node, self.add());
+            } else if self.consume(">") {
+                node = Node::new(NodeKind::Lt, self.add(), node);
+            } else if self.consume(">=") {
+                node = Node::new(NodeKind::Le, self.add(), node);
+            } else {
+                return node;
+            }
+        }
+    }
+
+    fn expect(&mut self, op: &str) {
         let token = self.token();
-        if token.kind != TokenKind::Reserved || token.str != op {
+        if token.kind != TokenKind::Reserved || token.str.to_string() != op {
+            panic!("expected: {}, actual: {}", op, token.str);
+        }
+        self.next();
+    }
+
+    fn consume(&mut self, op: &str) -> bool {
+        let token = self.token();
+        if token.kind != TokenKind::Reserved || token.str.to_string() != op {
             return false;
         }
         self.next();
@@ -182,14 +250,18 @@ impl Tokens {
 }
 
 impl Token {
-    fn new(kind: TokenKind, str: char) -> Self {
-        let tok = Self { kind, str };
+    fn new(kind: TokenKind, str: impl Into<String>) -> Self {
+        let tok = Self {
+            kind,
+            str: str.into(),
+        };
         tok
     }
 
     fn tokenize(p: String) -> Result<Vec<Token>, String> {
         let mut tokens = vec![];
         let chars = p.chars();
+        let chars_vec = p.chars().collect::<Vec<char>>();
         let mut chars_iter = chars.clone().enumerate();
 
         let mut index = None;
@@ -197,6 +269,30 @@ impl Token {
             index = Some(i);
             if p.is_whitespace() {
                 continue;
+            }
+
+            if let Some(index) = index {
+                let p_foo = format!(
+                    "{}{}",
+                    p,
+                    &chars_vec
+                        .get(index + 1)
+                        .map(|p| p.to_string())
+                        .unwrap_or_default()
+                );
+                log::debug!("p_foo={}", p_foo);
+                if p_foo.starts_with("==")
+                    || p_foo.starts_with("!=")
+                    || p_foo.starts_with("<=")
+                    || p_foo.starts_with(">=")
+                {
+                    let second = chars_iter.next();
+                    tokens.push(Token::new(
+                        TokenKind::Reserved,
+                        format!("{}{}", p, second.unwrap().1),
+                    ));
+                    continue;
+                }
             }
 
             if p == '+' || p == '-' || p == '*' || p == '/' || p == '(' || p == ')' {
@@ -224,7 +320,25 @@ impl Token {
                     p,
                 ));
                 if let Some(op) = op {
-                    tokens.push(Self::new(TokenKind::Reserved, op));
+                    let p_foo = format!(
+                        "{}{}",
+                        op,
+                        chars_vec
+                            .get(index.unwrap() + 1)
+                            .map(|p| p.to_string())
+                            .unwrap_or_default()
+                    );
+                    log::debug!("p_foo_2={}", p_foo);
+                    if p_foo == "==" || p_foo == "!=" || p_foo == "<=" || p_foo == ">=" {
+                        let second = chars_iter.next();
+                        tokens.push(Token::new(
+                            TokenKind::Reserved,
+                            format!("{}{}", op, second.unwrap().1),
+                        ));
+                        continue;
+                    } else {
+                        tokens.push(Self::new(TokenKind::Reserved, op));
+                    }
                 }
                 continue;
             };
@@ -240,7 +354,7 @@ impl Token {
             ));
         }
 
-        tokens.push(Self::new(TokenKind::Eof, '\0'));
+        tokens.push(Self::new(TokenKind::Eof, ""));
         Ok(tokens)
     }
 }
